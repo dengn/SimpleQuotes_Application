7f11273044aeed1e8bdfa6ea13b1730e93b59e3d
Richelle Guice <rguice@andrew.cmu.edu>
2022-07-18 11:12:38 -0400

    lots of new changes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,13 @@
 class Quote < ApplicationRecord
+    # Method to get a random quote for home page
+    # 
+    # @return [Quote]
+    #   the random quote 
+    def self.get_random_quote
+        self.all.sample
+    end
+
+    validates_presence_of :body
+    validates_presence_of :source
+    
 end
