b04a15d1a50a5c8a802791a90ab9c6a203998943
Richelle Guice <rguice@andrew.cmu.edu>
2022-07-18 11:03:30 -0400

    database working!!

--- db/migrate/20220712161250_create_quotes.rb
+++ db/migrate/20220712161250_create_quotes.rb
@@ -2,7 +2,7 @@ class CreateQuotes < ActiveRecord::Migration[5.2]
   def change
     create_table :quotes do |t|
       t.string :body
-      t.string :source
+      t.string :source, default: "Anonymous"
 
       t.timestamps
     end
